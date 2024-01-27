a51cac6641229a61b4246fd0c990445a5255a776
Raveenaa <147423011+RaveenaaDeenadayalan@users.noreply.github.com>
2024-01-27 13:51:07 -0600

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ console.log('Is this working?');
 let viz;
 
 //Add Share Link to Tableau Public in here
-const url = "https://public.tableau.com/views/covid19_USA/USACovid-19?:language=en-US&publish=yes&:display_count=n&:origin=viz_share_link";
+const url = "https://public.tableau.com/views/Tableauportfolio_17063248649010/HouseSales?:language=en-US&:display_count=n&:origin=viz_share_link";
 
 const vizContainer = document.getElementById('vizContainer');
 const options = {
